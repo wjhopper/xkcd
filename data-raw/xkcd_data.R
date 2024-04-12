45301e1341f7d6b95ac34e9432933147ac198517
Will Hopper <wjhopper510@gmail.com>
2024-04-12 08:38:56 -0400

    Included a data set of xkcd comic metadata describing the first 10 comics ever published.

---
+++ data-raw/xkcd_data.R
@@ -0,0 +1,16 @@
+## code to prepare `xkcd_data` dataset goes here
+
+library(jsonlite)
+
+json_objects <- vector(mode = "list", 10)
+
+for (i in 1:10) {
+  url <- file.path("https://xkcd.com", i, "info.0.json")
+  json_objects[[i]] <- jsonlite::read_json(url)
+  Sys.sleep(1)
+}
+
+xkcd <- do.call(rbind, json_objects)
+xkcd_data <- as.data.frame(xkcd)
+
+usethis::use_data(xkcd_data, overwrite = TRUE)
